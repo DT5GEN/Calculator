cbfdfdef18cbf0eac77d4c0fe396e46cb16b40fa
DT5Gen <marinovod@gmail.com>
2021-12-17 01:27:49 +0300

    v1.3.4 changed the template to ConstraintLayout

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -9,6 +9,6 @@ public class Calculator extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
+        setContentView(R.layout.activity_calculator_constraint_layout);
     }
 }
